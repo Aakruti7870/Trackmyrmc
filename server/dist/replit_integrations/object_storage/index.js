d8d25dca02669cd1c18c8c1d7dbfde94a2d2f9a4
Aakruti7870 <krushnabade54@gmail.com>
2026-08-04 00:25:40 +0000

    chore: remove accidentally tracked server/dist from git; add server/dist to .gitignore

--- server/dist/replit_integrations/object_storage/index.js
+++
@@ -1,3 +0,0 @@
-export { ObjectStorageService, ObjectNotFoundError, objectStorageClient, } from "./objectStorage.js";
-export { canAccessObject, getObjectAclPolicy, setObjectAclPolicy, } from "./objectAcl.js";
-export { registerObjectStorageRoutes } from "./routes.js";
